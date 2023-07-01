9fcf71cf75cd74b86908e1bbf43471a27ab135f4
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-01 17:35:27 +0900

    [ design ] : study RN Layout

--- App.tsx
+++ App.tsx
@@ -1,69 +1,30 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button, StatusBar, Vibration } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  StatusBar,
+  Vibration,
+} from "react-native";
+import BasicStudy from "./Pages/BasicStudy";
 
-interface Props {
-  name: string;
-  baseEnthusiasmLevel?: number;
-}
-
-function App({ name, baseEnthusiasmLevel = 0 }: Props) {
-  const [enthusiasmLevel, setEnthusiasmLevel] = useState(baseEnthusiasmLevel);
-
-  const onIncrement = () => {
-    setEnthusiasmLevel(enthusiasmLevel + 1);
-  };
-
-  const onDecrement = () => {
-    setEnthusiasmLevel(enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0);
-  };
-
-  const getExclamationMarks = (numChars: number) => {
-    return numChars > 0 ? "!".repeat(numChars) : "";
-  };
-
+function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.greeting}>
-        Hello World {name}
-        {getExclamationMarks(enthusiasmLevel)}
-      </Text>
-      <View>
-        <Button
-          title="Increase enthusiasm"
-          accessibilityLabel="increment"
-          onPress={onIncrement}
-          color="blue"
-        />
-        <Button
-          title="Decrease enthusiasm"
-          accessibilityLabel="decrement"
-          onPress={onDecrement}
-          color="red"
-        />
-        {/* Vibration Btn */}
-        <Button
-          title="Vibrate for 10 seconds"
-          onPress={() => Vibration.vibrate(10 * 100)}
-        />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+        <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+        <View style={{ flex: 1, backgroundColor: "orange" }}></View>
       </View>
-      <StatusBar barStyle={"light-content"} />
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+        <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      </View>
+      <BasicStudy name={"chanwoo"} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "purple",
-  },
-  greeting: {
-    color: "white",
-    fontSize: 20,
-    fontWeight: "bold",
-    margin: 16,
-  },
-});
 
 export default App;
---
+++ Pages/BasicStudy.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  StatusBar,
+  Vibration,
+} from "react-native";
+
+interface Props {
+  name: string;
+  baseEnthusiasmLevel?: number;
+}
+
+function BasicStudy({ name, baseEnthusiasmLevel = 0 }: Props) {
+  const [enthusiasmLevel, setEnthusiasmLevel] = useState(baseEnthusiasmLevel);
+
+  const onIncrement = () => {
+    setEnthusiasmLevel(enthusiasmLevel + 1);
+  };
+
+  const onDecrement = () => {
+    setEnthusiasmLevel(enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0);
+  };
+
+  const getExclamationMarks = (numChars: number) => {
+    return numChars > 0 ? "!".repeat(numChars) : "";
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.greeting}>
+        Hello World {name}
+        {getExclamationMarks(enthusiasmLevel)}
+      </Text>
+      <View>
+        <Button
+          title="Increase enthusiasm"
+          accessibilityLabel="increment"
+          onPress={onIncrement}
+          color="blue"
+        />
+        <Button
+          title="Decrease enthusiasm"
+          accessibilityLabel="decrement"
+          onPress={onDecrement}
+          color="red"
+        />
+        {/* Vibration Btn */}
+        <Button
+          title="Vibrate for 10 seconds"
+          onPress={() => Vibration.vibrate(10 * 100)}
+        />
+      </View>
+      <StatusBar barStyle={"light-content"} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position:"absolute",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "purple",
+  },
+  greeting: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 16,
+  },
+});
+
+export default BasicStudy;
