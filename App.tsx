cc9c8fde4e0b9895115d559f9c096edf928d8ac6
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-03 03:36:35 +0900

    [ finish ] : ReactNative Weather APP

--- App.tsx
+++ App.tsx
@@ -1,30 +1,167 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
+  Dimensions,
+  ActivityIndicator,
   StyleSheet,
-  Button,
-  StatusBar,
-  Vibration,
+  ScrollView,
 } from "react-native";
-import BasicStudy from "./Pages/BasicStudy";
+import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+interface IconProps {
+  Clouds: string;
+  Clear: string;
+  Atmosphere: string;
+  Snow: string;
+  Rain: string;
+  Drizzle: string;
+  Thunderstorm: string;
+}
+
+// { [key: string]: string } 이게 뭘까 아래 블로그 참고해서 해결하긴 함
+// https://ella951230.tistory.com/entry/%ED%83%80%EC%9E%85%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EC%97%90%EB%9F%AC-Type-undefined-cannot-be-used-as-an-index-type
+const icons: { [key: string]: string } = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 function App() {
+  const [city, setCity] = useState<string | null>("Loading...");
+  const [days, setDays] = useState<any[]>([]);
+  const [ok, setOk] = useState(true);
+
+  const API_KEY = "b1581c63833eb15d2ba7179d359626a0";
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].region);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+      // `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt=7&appid=${API_KEY}`
+    ).then((res) => {
+      return res.json();
+    });
+    setDays([response]);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-        <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-        <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-        <View style={{ flex: 1, backgroundColor: "orange" }}></View>
-        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      </View>
-      <BasicStudy name={"chanwoo"} />
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days?.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days?.map((day, index) => {
+            return (
+              <View key={index} style={styles.day}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    width: "100%",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Text style={styles.temp}>
+                    {parseFloat(day?.main?.temp).toFixed(1)}
+                  </Text>
+                  <Fontisto
+                    //언제 any 를 붙여야 할까?
+                    name={icons[day?.weather[0]?.main] as any}
+                    size={68}
+                    color="white"
+                  />
+                </View>
+                <Text style={styles.description}>{day?.weather[0].main}</Text>
+                <Text style={styles.tinyText}>
+                  {day?.weather[0].description}
+                </Text>
+              </View>
+            );
+          })
+        )}
+      </ScrollView>
     </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+    color: "white",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 100,
+    color: "white",
+  },
+  description: {
+    marginTop: -10,
+    fontSize: 30,
+    color: "white",
+    fontWeight: "500",
+  },
+  tinyText: {
+    marginTop: -5,
+    fontSize: 25,
+    color: "white",
+    fontWeight: "500",
+  },
+});
